064a6fd58e8bd40450af025d35938f9e90933663
Giggly Bits <71092239+greaselovely@users.noreply.github.com>
2021-08-14 17:41:51 -0600

    Update worktical.sh

--- worktical.sh
+++ worktical.sh
@@ -32,169 +32,169 @@ tput setaf 2
 tput civis
 
 if [[ $1 == "" ]]
-	then
-		sec=".3"
-	else
-		sec=$1
+        then
+                sec=".3"
+        else
+                sec=$1
 fi
 
 function eyetical(){
-	clear
-	eye="ICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgCiAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgIAogICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAKICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgCiAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgIAogICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAKICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgCiAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgIAogICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgIC4uLCw7Ozs7OzssLCwsICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAKICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAuLDsnJzs7LC4uLDs7OywsLCwsLicnJzs7LC4uICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgCiAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgLCwnJyAgICAgICAgICAgICAgICAgICAgJyc7Ozs7LDsnJyAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgIAogICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgOycgICAgLDtAQDsnICAsQEA7LCBAQCwgJzs7O0BAOyw7JzsuICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAKICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgJycgICw7QEBAQEAnICA7QEBAQDsgJycgICAgOztAQEBAQDs7OzsgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgCiAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgIDs7QEBAQEA7ICAgICcnJyAgICAgLiwsOzs7QEBAQEBAQDs7OyAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgIAogICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgIDs7QEBAQEBAOyAgICAgICAgICAgLCAnOzs7QEBAQEBAQEA7OzsuICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAKICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgJyc7QEBAQEAsLiAgLCAgIC4gICAnLDs7O0BAQEBAQDs7Ozs7OyAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgCiAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgIC4gICAnJzs7Ozs7Ozs7Oyw7Ozs7QEBAQEA7OycgLC46OycgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAKICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAnJy4uLCwgICAgICcnJycgICAgJyAgLiw7JyAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgCiAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAnJycnJyc6OicnJycnJycnICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgIAoKCg=="
-	base64 -d <<< $eye
-	sleep 2
+        clear
+        eye="CgoKCgoKCgoKCgogICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgIC4uLCw7Ozs7OzssLCwsCiAgICAgICAgICAgICAgICAgICAgICAgICAgLiw7Jyc7OywuLiw7OzssLCwsLC4nJyc7OywuLgogICAgICAgICAgICAgICAgICAgICAgICwsJycgICAgICAgICAgICAgICAgICAgICcnOzs7Oyw7JycKICAgICAgICAgICAgICAgICAgICAgIDsnICAgICw7QEA7JyAgLEBAOywgQEAsICc7OztAQDssOyc7LgogICAgICAgICAgICAgICAgICAgICAnJyAgLDtAQEBAQCcgIDtAQEBAOyAnJyAgICA7O0BAQEBAOzs7OwogICAgICAgICAgICAgICAgICAgICAgICA7O0BAQEBAOyAgICAnJycgICAgIC4sLDs7O0BAQEBAQEA7OzsKICAgICAgICAgICAgICAgICAgICAgICA7O0BAQEBAQDsgICAgICAgICAgICwgJzs7O0BAQEBAQEBAOzs7LgogICAgICAgICAgICAgICAgICAgICAgICAnJztAQEBAQCwuICAsICAgLiAgICcsOzs7QEBAQEBAOzs7Ozs7CiAgICAgICAgICAgICAgICAgICAgICAgICAgIC4gICAnJzs7Ozs7Ozs7Oyw7Ozs7QEBAQEA7OycgLC46OycKICAgICAgICAgICAgICAgICAgICAgICAgICAgICAnJy4uLCwgICAgICcnJycgICAgJyAgLiw7JwogICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgJycnJycnOjonJycnJycnJwoK"
+        base64 -d <<< $eye
+        sleep 2
 }
 
 function alientical(){
-	clear
-	alien="CgoKCgoKCgoKCgogICAgCQkJCQkgICAgICAjIyAgICAgICAgICAjIwogICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAjIyAgICAgICMjICAKICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICMjIyMjIyMjIyMjIyMjCiAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgIyMjIyAgIyMjIyMjICAjIyMjCiAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICMjIyMjIyMjIyMjIyMjIyMjIyMjIyMKICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgIyMgICMjIyMjIyMjIyMjIyMjICAjIyAKICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgIyMgICMjICAgICAgICAgICMjICAjIwogICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAjIyMjICAjIyMjCgoKCgoKCgoKCgoK"
-	base64 -d <<< $alien
-	sleep 2
+        clear
+        alien="CgoKCgoKCgoKCgogICAgCQkJCQkgICAgICAjIyAgICAgICAgICAjIwogICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAjIyAgICAgICMjICAKICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICMjIyMjIyMjIyMjIyMjCiAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgIyMjIyAgIyMjIyMjICAjIyMjCiAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICMjIyMjIyMjIyMjIyMjIyMjIyMjIyMKICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgIyMgICMjIyMjIyMjIyMjIyMjICAjIyAKICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgIyMgICMjICAgICAgICAgICMjICAjIwogICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAjIyMjICAjIyMjCgoKCgoKCgoKCgoK"
+        base64 -d <<< $alien
+        sleep 2
 }
 
 function ufotical(){
-	clear
+        clear
     ufo="CgoKCgoKCgoKCgogCQkJICAgICAgICAgICAgICAgICAgICAgIyMjIyMjIyMjIyMjCiAgICAgICAgICAgICAgCQkJCSAjIyMjIyMjIyMjIyMjIyMjIyMjIyAKICAgICAgICAgCQkJICAgICAgICMjIyMjIyMjIyMjIyMjIyMjIyMjIyMjIwoJCQkgICAgICAgICAgICAgIyMjIyAgIyMjIyAgIyMjIyAgIyMjIyAgIyMjIwoJCQkgICAgICAgICAgICMjIyMjIyMjIyMjIyMjIyMjIyMjIyMjIyMjIyMjIyMjIAoJCQkgICAgICAgICAgICAgICAjIyMjIyMgICAgIyMjIyAgICAjIyMjIyMKCQkJICAgICAgICAgICAgICAgICAjIyAgICAgICAgICAgICAgICAjIwoKCgoKCgoKCgoKCgo="
     base64 -d <<< $ufo
-	sleep 4
+        sleep 4
 }
 
 function deniedtical(){
-	acde="CgoKCgoKCgoKCgoKCgkJCSMjIyMjIyMjIyMjIyMjIyMjIyMjIyMjIyMjIyMjIyMjIyMjIyMjIyMjIyMjIyMjIyMjIyMjIyMjIyMjIwoJCQkjIyMjIyMjIyMjIyMjIyMjIyMjIyMjIyMjIyMjIyMjIyMjIyMjIyMjIyMjIyMjIyMjIyMjIyMjIyMjIyMKCQkJIyMjCUFDQ0VTUyBERU5JRUQJQUNDRVNTIERFTklFRAlBQ0NFU1MgREVOSUVECSAjIyMKCQkJIyMjIyMjIyMjIyMjIyMjIyMjIyMjIyMjIyMjIyMjIyMjIyMjIyMjIyMjIyMjIyMjIyMjIyMjIyMjIyMjCgkJCSMjIyMjIyMjIyMjIyMjIyMjIyMjIyMjIyMjIyMjIyMjIyMjIyMjIyMjIyMjIyMjIyMjIyMjIyMjIyMjIwoKCgoKCgoKCgoKCgoKCgoKCgoK"
-	acde=$(base64 -d <<< $acde)
-	i=1
-	tput setaf 1
-	clear
-	tput cup 10 0
-	while [[ $i -lt 5 ]] 
-		do
-			echo "$acde"
-			i=$(($i + 1))
-			sleep 1
-			clear
-			tput cup 10 0
-			sleep .7
-	done
-	tput setaf 2
+        acde="CgoKCgoKCgoKCgoKCgkJCSMjIyMjIyMjIyMjIyMjIyMjIyMjIyMjIyMjIyMjIyMjIyMjIyMjIyMjIyMjIyMjIyMjIyMjIyMjIyMjIwoJCQkjIyMjIyMjIyMjIyMjIyMjIyMjIyMjIyMjIyMjIyMjIyMjIyMjIyMjIyMjIyMjIyMjIyMjIyMjIyMjIyMKCQkJIyMjCUFDQ0VTUyBERU5JRUQJQUNDRVNTIERFTklFRAlBQ0NFU1MgREVOSUVECSAjIyMKCQkJIyMjIyMjIyMjIyMjIyMjIyMjIyMjIyMjIyMjIyMjIyMjIyMjIyMjIyMjIyMjIyMjIyMjIyMjIyMjIyMjCgkJCSMjIyMjIyMjIyMjIyMjIyMjIyMjIyMjIyMjIyMjIyMjIyMjIyMjIyMjIyMjIyMjIyMjIyMjIyMjIyMjIwoKCgoKCgoKCgoKCgoKCgoKCgoK"
+        acde=$(base64 -d <<< $acde)
+        i=1
+        tput setaf 1
+        clear
+        tput cup 10 0
+        while [[ $i -lt 5 ]] 
+                do
+                        echo "$acde"
+                        i=$(($i + 1))
+                        sleep 1
+                        clear
+                        tput cup 10 0
+                        sleep .7
+        done
+        tput setaf 2
 
 }
 
 function grantical(){
-	pegr="CgoKCgoKCgoKCgoKCgkJCSMjIyMjIyMjIyMjIyMjIyMjIyMjIyMjIyMjIyMjIyMjIyMjIyMjIyMjIyMjIyMjIyMjIyMjIyMjIyMjIwoJCQkjIyMjIyMjIyMjIyMjIyMjIyMjIyMjIyMjIyMjIyMjIyMjIyMjIyMjIyMjIyMjIyMjIyMjIyMjIyMjIyMKCQkJIyMjCVBFUk1JU1NJT04gR1JBTlRFRCAtIFFVRVJZIFJVTk5JTkcgLSBTVEFOREJZCSAjIyMKCQkJIyMjIyMjIyMjIyMjIyMjIyMjIyMjIyMjIyMjIyMjIyMjIyMjIyMjIyMjIyMjIyMjIyMjIyMjIyMjIyMjCgkJCSMjIyMjIyMjIyMjIyMjIyMjIyMjIyMjIyMjIyMjIyMjIyMjIyMjIyMjIyMjIyMjIyMjIyMjIyMjIyMjIwoKCgoKCgoKCgoKCgoKCgoKCgoK"
-	pegr=$(base64 -d <<< $pegr)
-	tput setaf 7
-	clear
-	tput cup 10 0
-	echo "$pegr"
-	sleep 5
-	tput setaf 2
+        pegr="CgoKCgoKCgoKCgoKCgkJCSMjIyMjIyMjIyMjIyMjIyMjIyMjIyMjIyMjIyMjIyMjIyMjIyMjIyMjIyMjIyMjIyMjIyMjIyMjIyMjIwoJCQkjIyMjIyMjIyMjIyMjIyMjIyMjIyMjIyMjIyMjIyMjIyMjIyMjIyMjIyMjIyMjIyMjIyMjIyMjIyMjIyMKCQkJIyMjCVBFUk1JU1NJT04gR1JBTlRFRCAtIFFVRVJZIFJVTk5JTkcgLSBTVEFOREJZCSAjIyMKCQkJIyMjIyMjIyMjIyMjIyMjIyMjIyMjIyMjIyMjIyMjIyMjIyMjIyMjIyMjIyMjIyMjIyMjIyMjIyMjIyMjCgkJCSMjIyMjIyMjIyMjIyMjIyMjIyMjIyMjIyMjIyMjIyMjIyMjIyMjIyMjIyMjIyMjIyMjIyMjIyMjIyMjIwoKCgoKCgoKCgoKCgoKCgoKCgoK"
+        pegr=$(base64 -d <<< $pegr)
+        tput setaf 7
+        clear
+        tput cup 10 0
+        echo "$pegr"
+        sleep 5
+        tput setaf 2
 }
 
 function readdowntical(){
-	tput cup 0 0
-	read row col <<< $(stty size)
-	nol=$(cat $log | wc -l)
-	startat=$(printf "shuf -i 1-%d -n 1" ${nol})
-	startat=$($startat)
-	endat=$(($startat + $row))
-	readlog=$(sed -n -e ${startat},${endat}p ${log})
-	IFS=$'\n'
-	for line in $readlog
-		do
-			echo "$line"
-			sleep $sec
-	done
-	unset IFS
+        tput cup 0 0
+        read row col <<< $(stty size)
+        nol=$(cat $log | wc -l)
+        startat=$(printf "shuf -i 1-%d -n 1" ${nol})
+        startat=$($startat)
+        endat=$(($startat + $row))
+        readlog=$(sed -n -e ${startat},${endat}p ${log})
+        IFS=$'\n'
+        for line in $readlog
+                do
+                        echo "$line"
+                        sleep $sec
+        done
+        unset IFS
 }
 
 
 function searchtical(){
-	clear
-	read row col <<< $(stty size)
-	row=$(($row - 3))
-	# choose random color
-	tput setaf $(shuf -i 0-9 -n 1)
-	nol=$(cat $log | wc -l)
-	# choose a random line from $log
-	# on MacOS I had a problem of using a variable inside the shuf command
-	# where it normally worked on RHEL.  Lame.  
-	# So I use printf to dump it in.  Seems to work really well to my surprise.
-	shuffle=$(printf "shuf -i 1-%d -n 1" $nol )
-	string=$(sed -n $($shuffle)p $log)
-	# from that random line, grab the last word with any characters
-	# and grep $log and display the result
-	# Also found an issue that the script stops if the grep command somehow gets 
-	# into a blank character.  So I'm just checking it if it exists, and running
-	# another function just to keep things going.  Not heavily tested.
-	if [[ $string ]]
-		then
-			grep ${string##* } $log
-		else
-			grantical
-	fi
-	sleep 3
-	tput setaf 2
+        clear
+        read row col <<< $(stty size)
+        row=$(($row - 3))
+        # choose random color
+        tput setaf $(shuf -i 0-9 -n 1)
+        nol=$(cat $log | wc -l)
+        # choose a random line from $log
+        # on MacOS I had a problem of using a variable inside the shuf command
+        # where it normally worked on RHEL.  Lame.  
+        # So I use printf to dump it in.  Seems to work really well to my surprise.
+        shuffle=$(printf "shuf -i 1-%d -n 1" $nol )
+        string=$(sed -n $($shuffle)p $log)
+        # from that random line, grab the last word with any characters
+        # and grep $log and display the result
+        # Also found an issue that the script stops if the grep command somehow gets 
+        # into a blank character.  So I'm just checking it if it exists, and running
+        # another function just to keep things going.  Not heavily tested.
+        if [[ $string ]]
+                then
+                        grep ${string##* } $log
+                else
+                        grantical
+        fi
+        sleep 3
+        tput setaf 2
 }
 
 function hextical(){
-	tput cup 0 0
-	for file in $(find ${HOME} -maxdepth 1 -type f | sort -R)
-		do
-			head -c 4096 $file | hexdump -C
-			sleep $sec
-	done
+        tput cup 0 0
+        for file in $(find ${HOME} -maxdepth 1 -type f | sort -R)
+                do
+                        head -c 4096 $file | hexdump -C
+                        sleep $sec
+        done
 }
 
 function statical(){
-	for file in $(find ${HOME} -maxdepth 1 -type f)
-		do
-			clear
-			tput cup 10 0
-			stat $file
-			sleep $sec
-	done
+        for file in $(find ${HOME} -maxdepth 1 -type f)
+                do
+                        clear
+                        tput cup 10 0
+                        stat $file
+                        sleep $sec
+        done
 }
 
 function readuptical(){
-	read row col <<< $(stty size)
-	row=$(($row - 3	))
-	nol=$(cat $log | wc -l)
-	startat=$(printf "shuf -i 1-%d -n 1" $nol)
-	startat=$($startat)
-	endat=$(($startat + $row))
-	shuffle=$(printf "sed -n -e %d,%d" ${startat} ${endat})
-	readlog=$(${shuffle}p ${log})
-	IFS=$'\n'
-	clear
-	for line in $readlog
-		do
-			tput cup $row 0
-			echo "$line"
-			sleep $sec
-			row=$(($row - 1))
-			# this can be annoying or cool, uncomment if you want random colors per line
-			#tput setaf $(shuf -i 0-9 -n 1)
-	done
-	unset IFS
+        read row col <<< $(stty size)
+        row=$(($row - 3 ))
+        nol=$(cat $log | wc -l)
+        startat=$(printf "shuf -i 1-%d -n 1" $nol)
+        startat=$($startat)
+        endat=$(($startat + $row))
+        shuffle=$(printf "sed -n -e %d,%d" ${startat} ${endat})
+        readlog=$(${shuffle}p ${log})
+        IFS=$'\n'
+        clear
+        for line in $readlog
+                do
+                        tput cup $row 0
+                        echo "$line"
+                        sleep $sec
+                        row=$(($row - 1))
+                        # this can be annoying or cool, uncomment if you want random colors per line
+                        #tput setaf $(shuf -i 0-9 -n 1)
+        done
+        unset IFS
 
 }
 
 function loadtical(){
-	read row col <<< $(stty size)
-	# this will determine the halfway point across the screen
-	# as the loading bar will only go to the middle of the screen
-	half=$(($col / 2))
-	clear
-	tput cup 9 0
-	printf "%30s\n" "Loading..."
-	i=0
-	while [[ $i -lt $half ]]
-		do
-			printf "#"
-			tput cup 10 $i
-			sleep .1
-			i=$(($i + 1))
-	done
+        read row col <<< $(stty size)
+        # this will determine the halfway point across the screen
+        # as the loading bar will only go to the middle of the screen
+        half=$(($col / 2))
+        clear
+        tput cup 9 0
+        printf "%30s\n" "Loading..."
+        i=0
+        while [[ $i -lt $half ]]
+                do
+                        printf "#"
+                        tput cup 10 $i
+                        sleep .1
+                        i=$(($i + 1))
+        done
 }
 
 
@@ -206,10 +206,9 @@ function loadtical(){
 functical=$(declare -F | awk '{print $3}')
 
 while true
-	do
-		# we take all of the functions and shuffle them so
-		# that the output is random
-		stuffandthings=$(shuf -e $functical)
-		$stuffandthings
+        do
+                # we take all of the functions and shuffle them so
+                # that the output is random
+                stuffandthings=$(shuf -e $functical)
+                $stuffandthings
 done
-
