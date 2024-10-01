8d45ffbe678ff9e9bebed8f405a9889c9081495b
Shayne Sivley <shayne@fennon.com>
2024-10-01 12:56:02 -0600

    memtical

--- worktical.sh
+++ worktical.sh
@@ -215,7 +215,6 @@ memtical() {
                        NR==2 {printf "%-16s % 16.2f Mi\n", "Used:", $3}
                        NR==2 {printf "%-16s % 16.2f Mi\n", "Total:", $2}'
     fi
-    sleep "${SLEEP_TIME:-1}"  # Default to 1 second if SLEEP_TIME is not set
 }
 
 # Function to display IP addresses
